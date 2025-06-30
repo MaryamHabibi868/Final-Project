4dfccdfb8b1d0ba55ca1c1c6f68e68eff2498e72
Maryam <maryamhabibi868@gmail.com>
2025-06-30 18:43:48 +0330

    feat: add crudRepository class

---
+++ src/main/java/ir/maktab/homeservice/repository/base/CrudRepository.java
@@ -0,0 +1,21 @@
+package ir.maktab.homeservice.repository.base;
+
+import ir.maktab.homeservice.domains.base.BaseEntity;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+public interface CrudRepository
+<T extends BaseEntity> {
+
+    T save(T entity);
+    List<T> saveAll(Collection<T> entities);
+    Optional<T> findById(Long id);
+    List<T> findAll();
+    List<T> findAllById(Iterable<Long> ids);
+    Long countAll();
+    void deleteById(Long id);
+    void deleteAllById(Iterable<Long> ids);
+    boolean existsById(Long id);
+
+}
