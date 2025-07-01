d0b273dd37f7d10045c7c1c776ffc3dc83e41a35
Maryam <maryamhabibi868@gmail.com>
2025-07-01 23:56:53 +0330

    feat: add create and update method in MainServiceServiceImpl

--- src/main/java/ir/maktab/homeservice/service/MainServiceServiceImpl.java
+++ src/main/java/ir/maktab/homeservice/service/MainServiceServiceImpl.java
@@ -1,15 +1,41 @@
 package ir.maktab.homeservice.service;
 
 import ir.maktab.homeservice.domains.MainService;
+import ir.maktab.homeservice.dto.MainServiceSaveUpdateRequest;
+import ir.maktab.homeservice.exception.DuplicatedException;
+import ir.maktab.homeservice.exception.NotFoundException;
 import ir.maktab.homeservice.repository.MainServiceRepository;
-
 import ir.maktab.homeservice.service.base.BaseServiceImpl;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class MainServiceServiceImpl
         extends BaseServiceImpl<MainService, MainServiceRepository>
-        implements MainServiceService{
+        implements MainServiceService {
 
     public MainServiceServiceImpl(MainServiceRepository repository) {
         super(repository);
+    }
+
+    public MainService createMainService(MainServiceSaveUpdateRequest request) {
+       Optional<MainService> foundMainService = repository.findByMainServiceTitle(request.getMainServiceTitle());
+        if (foundMainService.isPresent()) {
+            throw new DuplicatedException("Main Service Title Already Exists");
+        }
+        MainService mainService = new MainService();
+        mainService.setMainServiceTitle(request.getMainServiceTitle());
+        return repository.save(mainService);
+    }
+
+    public MainService updateMainService(MainServiceSaveUpdateRequest request) {
+        Optional<MainService> foundMainService = repository.findById(request.getId());
+        if (foundMainService.isPresent()) {
+            MainService mainService = new MainService();
+            mainService.setMainServiceTitle(request.getMainServiceTitle());
+           return repository.save(mainService);
+        }
+        throw new NotFoundException("Main Service Not Found");
     }
 }
