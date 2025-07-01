8c53e5b62112a4bb70e20235501c4c0a162cbcdc
Maryam <maryamhabibi868@gmail.com>
2025-07-01 23:54:15 +0330

    feat: add exception layer

---
+++ src/main/java/ir/maktab/homeservice/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package ir.maktab.homeservice.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
