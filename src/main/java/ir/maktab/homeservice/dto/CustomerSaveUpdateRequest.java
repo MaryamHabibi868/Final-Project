e6bf841400556be33bd084cf1126dfe76a716346
Maryam <maryamhabibi868@gmail.com>
2025-07-02 18:33:02 +0330

    feat: add methods to CustomerServiceImpl for registration and login

---
+++ src/main/java/ir/maktab/homeservice/dto/CustomerSaveUpdateRequest.java
@@ -0,0 +1,39 @@
+package ir.maktab.homeservice.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerSaveUpdateRequest {
+
+    @NotNull(groups = {ValidationGroup.Update.class})
+    @Null(groups = {ValidationGroup.Save.class})
+    private Long id;
+
+    @NotBlank(groups = {ValidationGroup.Save.class},
+            message = "First name should be entered.")
+    private String firstName;
+
+    @NotBlank(groups = {ValidationGroup.Save.class},
+            message = "Last name should be entered.")
+    private String lastName;
+
+    @NotBlank(groups = {ValidationGroup.Save.class},
+            message = "Email should be entered.")
+    @Email
+    private String email;
+
+    @NotBlank(groups = {ValidationGroup.Save.class},
+            message = "Password should be entered.")
+    @Size(min = 8, max = 20, message = "Password should be between 8-20 character ")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password should be combination of letters and numbers"
+    )
+    private String password;
+
+}
--- src/main/java/ir/maktab/homeservice/service/CustomerServiceImpl.java
+++ src/main/java/ir/maktab/homeservice/service/CustomerServiceImpl.java
@@ -1,16 +1,78 @@
 package ir.maktab.homeservice.service;
 
 import ir.maktab.homeservice.domains.Customer;
+import ir.maktab.homeservice.dto.CustomerSaveUpdateRequest;
+import ir.maktab.homeservice.exception.NotFoundException;
 import ir.maktab.homeservice.repository.CustomerRepository;
 import ir.maktab.homeservice.service.base.BaseServiceImpl;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CustomerServiceImpl
         extends BaseServiceImpl<Customer, CustomerRepository>
-        implements CustomerService{
+        implements CustomerService {
 
     public CustomerServiceImpl(CustomerRepository repository) {
         super(repository);
+    }
+
+    public Customer registerCustomer(CustomerSaveUpdateRequest request) {
+        Customer customer = new Customer();
+        customer.setFirstName(request.getFirstName());
+        customer.setLastName(request.getLastName());
+        customer.setEmail(request.getEmail());
+        customer.setPassword(request.getPassword());
+        return repository.save(customer);
+    }
+
+    public Customer updateCustomer(CustomerSaveUpdateRequest request) {
+        Optional<Customer> foundCustomer = repository.findById(request.getId());
+        if (foundCustomer.isEmpty()) {
+            throw new NotFoundException("Customer not found");
+        }
+        Customer customer = new Customer();
+        customer.setFirstName(foundCustomer.get().getFirstName());
+        customer.setLastName(foundCustomer.get().getLastName());
+        customer.setEmail(foundCustomer.get().getEmail());
+        customer.setPassword(foundCustomer.get().getPassword());
+        return repository.save(customer);
+    }
+
+    public void deleteCustomer(CustomerSaveUpdateRequest request) {
+        Optional<Customer> foundCustomer = repository.findById(request.getId());
+        if (foundCustomer.isEmpty()) {
+            throw new NotFoundException("Customer not found");
+        }
+        repository.deleteById(request.getId());
+    }
+
+    public List<Customer> findAllCustomers() {
+        return repository.findAll();
+    }
+
+    public Customer customerRegistration(CustomerSaveUpdateRequest request) {
+        Customer customer = new Customer();
+        customer.setFirstName(request.getFirstName());
+        customer.setLastName(request.getLastName());
+        customer.setEmail(request.getEmail());
+        customer.setPassword(request.getPassword());
+        return repository.save(customer);
+    }
+
+    public Customer customerLogin(CustomerSaveUpdateRequest request) {
+        Customer customer = new Customer();
+        Optional<Customer> foundCustomer = repository.findByEmail(request.getEmail());
+        if (foundCustomer.isPresent()) {
+            if (foundCustomer.get().getPassword().equals(request.getPassword())) {
+                System.out.println("Login Success");
+                return foundCustomer.get();
+            } else {
+                System.out.println("Wrong Password");
+            }
+        }
+        throw new NotFoundException("Customer not found");
     }
 }
