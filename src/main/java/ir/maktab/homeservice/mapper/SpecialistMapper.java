31a7e942050c56987e467fc15cf31fb967e28ab8
Maryam <maryamhabibi868@gmail.com>
2025-07-06 22:20:53 +0330

    feat: add mapper class for specialist

---
+++ src/main/java/ir/maktab/homeservice/mapper/SpecialistMapper.java
@@ -0,0 +1,12 @@
+package ir.maktab.homeservice.mapper;
+
+import ir.maktab.homeservice.domains.Specialist;
+import ir.maktab.homeservice.dto.SpecialistSaveUpdateRequest;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface SpecialistMapper {
+
+    SpecialistSaveUpdateRequest specialistMapToDTO(Specialist specialist);
+
+    Specialist specialistDTOMapToEntity(SpecialistSaveUpdateRequest specialistSaveUpdateRequest);
+}
