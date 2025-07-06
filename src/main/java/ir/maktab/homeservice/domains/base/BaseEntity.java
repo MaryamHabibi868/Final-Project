b6b62eb7f77550555121225333a37c063818d7f1
Maryam <maryamhabibi868@gmail.com>
2025-07-06 14:46:22 +0330

    refactor: refactor BaseEntity class and add boolean is active for data in database

--- src/main/java/ir/maktab/homeservice/domains/base/BaseEntity.java
+++ src/main/java/ir/maktab/homeservice/domains/base/BaseEntity.java
@@ -20,6 +20,7 @@ public class BaseEntity<ID extends Serializable> implements Serializable {
     public static final String ID = "id";
     public static final String CREATE_DATE = "create_date";
     public static final String LAST_UPDATE_DATE = "last_update_date";
+    public static final String IS_ACTIVE = "is_active";
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
@@ -33,15 +34,12 @@ public class BaseEntity<ID extends Serializable> implements Serializable {
     @LastModifiedDate
     private ZonedDateTime lastUpdateDate;
 
-    private Boolean active = true;
-
-   /* @PrePersist
-    public void isActive() {
-        this.active = true;
-    }*/
+    @Column(name = IS_ACTIVE)
+    private Boolean isActive;
 
     @PrePersist
     public void registrationDate() {
         createDate = ZonedDateTime.now();
+        isActive = true;
     }
 }
