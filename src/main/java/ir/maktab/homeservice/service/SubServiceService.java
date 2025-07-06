82f1f0ed0ec3f0d865b21058ceca00a8ff60d27d
Maryam <maryamhabibi868@gmail.com>
2025-07-06 22:15:11 +0330

    delete useless classes

--- src/main/java/ir/maktab/homeservice/service/SubServiceService.java
+++
@@ -1,7 +0,0 @@
-package ir.maktab.homeservice.service;
-
-import ir.maktab.homeservice.domains.SubService;
-import ir.maktab.homeservice.service.base.BaseService;
-
-public interface SubServiceService extends BaseService<SubService, Long> {
-}
