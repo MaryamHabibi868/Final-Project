2412c18ff3019b66a8cc6322b21788b75f38c268
Maryam <maryamhabibi868@gmail.com>
2025-07-07 21:29:36 +0330

    feat: add methods in BaseService and BaseServiceImpl to use in service layer

--- src/main/java/ir/maktab/homeservice/service/base/BaseService.java
+++ src/main/java/ir/maktab/homeservice/service/base/BaseService.java
@@ -1,8 +1,14 @@
 package ir.maktab.homeservice.service.base;
 
 import ir.maktab.homeservice.domains.base.BaseEntity;
+import lombok.RequiredArgsConstructor;
+
 import java.io.Serializable;
+import java.util.Optional;
 
 public interface BaseService <T extends BaseEntity<ID> , ID extends Serializable> {
 
+    Optional<T> findById(ID id);
+
+    T save(T entity);
 }
--- src/main/java/ir/maktab/homeservice/service/base/BaseServiceImpl.java
+++ src/main/java/ir/maktab/homeservice/service/base/BaseServiceImpl.java
@@ -2,11 +2,32 @@ package ir.maktab.homeservice.service.base;
 
 import ir.maktab.homeservice.domains.base.BaseEntity;
 import ir.maktab.homeservice.repository.base.BaseRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 import java.io.Serializable;
+import java.util.Optional;
 
 public class BaseServiceImpl
         <T extends BaseEntity<ID>, ID extends Serializable,
                 R extends BaseRepository<T, ID>>
-        implements BaseService<T, ID>{
+        implements BaseService<T, ID> {
 
+    protected final R repository;
+
+    public BaseServiceImpl(R repository) {
+        this.repository = repository;
+    }
+
+
+    @Override
+    public Optional<T> findById(ID id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public T save(T entity) {
+        return repository.save(entity);
+    }
 }
